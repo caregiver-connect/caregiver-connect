d14a9a3ef4882b21cf34dcb0c88c07aab4aa9060
jwcurren <jwcurren@crimson.ua.edu>
2024-03-19 15:14:29 -0500

    GET requests

--- src/views/ProvidersSearch.vue
+++ src/views/ProvidersSearch.vue
@@ -1,3 +1,6 @@
+<!-- To do: 1. add padding at right of table.
+            2. vertically center cells-->
+
 <template>
   <ion-page>
     <ion-header>
@@ -10,7 +13,36 @@
           style="position: right 5px center; width: 35px;"></ion-img>
       </ion-toolbar>
     </ion-header>
-    <ion-content ref="content">
+    <ion-content :scroll-x=true>
+      <ion-searchbar></ion-searchbar>
+      <ion-grid>
+        <ion-row class="header-row">
+          <ion-col class="header-col" size="1">ID</ion-col>
+          <ion-col class="header-col" size="2">Provider Name</ion-col>
+          <ion-col class="header-col" size="2">Address</ion-col>
+          <ion-col class="header-col" size="2">Address Line 2</ion-col>
+          <ion-col class="header-col" size="3">City</ion-col>
+          <ion-col class="header-col" size="1">State</ion-col>
+          <ion-col class="header-col" size="2">zip</ion-col>
+          <ion-col class="header-col" size="2">county</ion-col>
+          <ion-col class="header-col" size="3">phone #</ion-col>
+          <ion-col class="header-col" size="4">website</ion-col>
+          <ion-col class="header-col" size="3">resources</ion-col>
+        </ion-row>
+        <ion-row v-for="entry in entries" :key="entry.id">
+          <ion-col size = "1">{{ entry.id_cms_other }}</ion-col>
+          <ion-col size = "2">{{ entry.agency_name }}</ion-col>
+          <ion-col size = "2">{{ entry.addr1 }}</ion-col>
+          <ion-col size = "2">{{ entry.addr2 }}</ion-col>
+          <ion-col size = "3">{{ entry.city }}</ion-col>
+          <ion-col size = "1">{{ entry.state }}</ion-col>
+          <ion-col size = "2">{{ entry.zip }}</ion-col>
+          <ion-col size = "2">{{ entry.county }}</ion-col>
+          <ion-col size = "3">{{ entry.phone_number }}</ion-col>
+          <ion-col size = "4">{{ entry.website }}</ion-col>
+          <ion-col size = "3">{{ entry.resources }}</ion-col>
+        </ion-row>
+      </ion-grid>
       <ion-fab vertical="bottom" horizontal="end" slot="fixed">
         <ion-fab-button @click="() => router.push('/add-provider')" color="crimson">
           <ion-icon :icon="add"></ion-icon>
@@ -20,7 +52,7 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import {
   IonBackButton,
   IonButtons,
@@ -32,12 +64,116 @@ import {
   IonFab,
   IonFabButton,
   IonIcon,
-  IonImg
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonSearchbar
 } from '@ionic/vue';
+import axios from 'axios';
 import { ref } from 'vue';
-import { add } from 'ionicons/icons';
+import { add, arrowUp, arrowDown } from 'ionicons/icons';
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
-const content = ref();
+interface Entry {
+  id_cms_other: number;
+  agency_name: string;
+  addr1: string;
+  addr2: string;
+  city: string;
+  state: string;
+  zip: string;
+  county: string;
+  phone_number: string;
+  website: string;
+  resources: string;
+}
+
+
+export default {
+  components: {
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonImg,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonSearchbar
+  },
+  data() {
+    return {
+      entries: [] as Entry[],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData(this: {entries: Entry[]}) {
+      try {
+        const response = await axios.get('http://' + self.location.hostname + ':8081/api/providers');
+        this.entries = response.data;
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+  setup() {
+    const router = useRouter();
+    return { add, arrowUp, arrowDown, router }
+  }
+}
 </script>
+
+<style scoped>
+ion-grid {
+  padding-left: 10px;
+  padding-bottom: 80px;
+  min-width: 600px;
+}
+
+ion-row {
+  /* keep rows from wrapping */
+  flex-wrap: nowrap;
+}
+
+
+.header-row {
+  color: #ffffff;
+
+  /* keep header row at top of screen when scroll */
+  position: sticky;
+  top: 0px;
+  left: 0px;
+  right: 0;
+  z-index: 9999;
+}
+
+.header-col {
+  background-color: #9e1b32;
+}
+
+.even {
+  background-color: lightgray;
+}
+
+ion-col {
+  border: solid 1px;
+  text-align: center;
+  align-self: stretch;
+}
+
+ion-fab {
+  padding: 1%;
+}
+</style>
