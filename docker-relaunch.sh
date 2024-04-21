7772a1b29b9cb6a7d4abd08a51b2b76df7dfebbb
Nallaka <mithul0700@gmail.com>
2024-04-21 12:09:07 -0500

    password reset

--- docker-relaunch.sh
+++ docker-relaunch.sh
@@ -6,12 +6,6 @@ docker rmi -f $(docker images -aq)
 
 docker-compose up --build --force-recreate --no-deps
 
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pdGh1bDA3MDBAZ21haWwuY29tIiwiaWF0IjoxNzEzMjk3NDk5LCJleHAiOjE3MTMzMDEwOTl9.OswT5cuD4u9nKgIaBLRYtb_qHLfEqrosP2twObzpBCk","user_id": "mithul"}' \
-  http://localhost:8081/api/email/verify-email
+curl -X POST -H "Content-Type: application/json" -d '{"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pdGh1bDA3MDBAZ21haWwuY29tIiwiaWF0IjoxNzEzNzE2MDU3LCJleHAiOjE3MTM3MTk2NTd9.L-WbIX1iIeSxo-YgOIsFzwxOp7Nj8W0kEBAwbd6rmXE","user_id": "mithul"}' http://localhost:8081/api/email/verify-email
 
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d '{"email": "mithul0700@gmail.com","user_id": "mithul"}' \
-  http://localhost:8081/api/email/send-verification-email
+curl -X POST -H "Content-Type: application/json" -d '{"email": "mithul0700@gmail.com","user_id": "mithul"}' http://localhost:8081/api/email/send-verification-email
--- provider-nodejs/app/controllers/email.controller.js
+++ provider-nodejs/app/controllers/email.controller.js
@@ -37,7 +37,7 @@ exports.sendVerificationEmail = (req, res) => {
 }
 
 exports.verifyEmail = (req, res) => {
-    const { token, user_id } = req.query;
+    const { token, user_id } = req.body;
 
     if (!token) {
         return res.status(400).send('Token is required.');
@@ -57,7 +57,7 @@ exports.verifyEmail = (req, res) => {
         .then(num => {
             if (num == 1) {
                 res.send({
-                    message: "User email verified successfully successfully!"
+                    message: "User email verified successfully!"
                 });
             } else {
                 res.send({
@@ -67,11 +67,81 @@ exports.verifyEmail = (req, res) => {
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating User with =username" + user_id
+                message: "Error updating User with username" + user_id
             });
         });
 
         // For demonstration purposes, just send a success response
-        res.send(`Email ${email} verified successfully.`);
-    }); 
+        // res.send(`Email ${email} verified successfully.`);
+    });
+}
+
+exports.sendVerificationEmail = (req, res) => {
+
+    const { email, user_id } = req.body;
+
+    const verification_token = generateToken(email);
+
+    const msg = {
+        to: email, // Change to your recipient
+        from: 'caregiver-connect-test@outlook.com', // Change to your verified sender
+        subject: 'Verify Your New Caregiver Connect Account',
+        text: `To verify your new Cargiver Connect account, please follow the verification link below:\nClick the following link to verify your email: http://cs495-spring2024-09.ua.edu:8081/api/email/verify-email?token=${verification_token}&user=${user_id}\nThis link will expire in 1 hour.`
+    }
+}
+
+exports.sendPasswordResetEmail = (req, res) => {
+
+    const { email, user_id } = req.body;
+
+    const verification_token = generateToken(email);
+
+    const msg = {
+        to: email, // Change to your recipient
+        from: 'caregiver-connect-test@outlook.com', // Change to your verified sender
+        subject: 'Reset your Caregiver Connect Password',
+        text: `To reset your Cargiver Connect account password, please follow the reset link below:\nClick the following link to verify your email: http://cs495-spring2024-09.ua.edu/reset-password?token=${verification_token}&user=${user_id}\nThis link will expire in 1 hour.`
+    }
+
+}
+
+exports.resetPassword = (req, res) => {
+
+    const { token, user_id, password } = req.body;
+
+    if (!token) {
+        return res.status(400).send('Token is required.');
+    }
+
+    // Verify the token
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            console.error('Error verifying token:', err);
+            return res.status(401).send('Invalid or expired token.');
+        }
+
+        // Extract necessary information from the decoded token
+        const { email } = decoded;
+
+        User.update({ password: "password" }, { where: { username: user_id } })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "User password updated successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot update User with username=${user_id}. Maybe User was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating User with username" + user_id
+            });
+        });
+
+        // For demonstration purposes, just send a success response
+        // res.send(`Email ${email} verified successfully.`);
+    });
 }
